ed5ca36f74921565e0e9ea0577234d10a32d2dba
Daniil <prestig.rd85@gmail.com>
2021-09-17 18:59:12 +0300

    animation

--- client/src/components/TickerItem/Ticker.jsx
+++ client/src/components/TickerItem/Ticker.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./Ticker.module.css";
 import classNames from 'classnames';
 import arrowDown from "../../assets/images/arrowDown.svg"
 import arrowTop from "../../assets/images/arrowTop.svg"
 
-const Ticker = ({ticker, price, change, change_percent}) => {
+const Ticker = (props) => {
+    const {ticker, price, change, change_percent} = props;
+    const [animationClass, setAnimationClass] = useState([classNames(styles.imgShowUpFromTop), classNames(styles.imgShowUpFromBottom), classNames(styles.spanShowUpFromTop)])
+
+    useEffect(() => {
+        setAnimationClass([classNames(styles.imgShowUpFromTop), classNames(styles.imgShowUpFromBottom), classNames(styles.spanShowUpFromTop)])
+        setTimeout(() => {
+            setAnimationClass(['', '', ''])
+        }, 1000)
+    }, [props])
+
 
     return (<div className={classNames(styles.tickerItem)}>
-        <div className={classNames(styles.tickerItem__img, change_percent > 0 ? styles.tickerItem__img_positive : styles.tickerItem__img_negative)}>
-            <img src={change_percent > 0 ? arrowTop : arrowDown} alt=""/>
+        <div
+            className={classNames(styles.tickerItem__img, change_percent > 0 ? styles.tickerItem__img_positive : styles.tickerItem__img_negative)}>
+            {change_percent > 0 ? <img className={animationClass[1]} src={arrowTop} alt=""/> :
+                <img className={animationClass[0]} src={arrowDown} alt=""/>}
         </div>
         <div className={classNames(styles.tickerItem__description)}>
             <div className={classNames(styles.description__topLine)}>
                 <span className={classNames(styles.description__ticker)}>{ticker}</span>
                 <span
-                    className={classNames(styles.description__changePercent, change_percent > 0 ? styles.positiveValue : styles.negativeValue)}>
+                    className={classNames(styles.description__changePercent, change_percent > 0 ? styles.positiveValue : styles.negativeValue) + ' ' + animationClass[2]}>
                     {change_percent > 0 ? '+' + change_percent : change_percent}%
                 </span>
             </div>
             <div className={classNames(styles.description__bottomLine)}>
-                <span className={classNames(styles.description__price)}>{price}</span>
+                <span className={classNames(styles.description__price) + ' ' + animationClass[2]}>{price}</span>
                 <span
-                    className={classNames(styles.description__change, change > 0 ? styles.positiveValue : styles.negativeValue)}>
+                    className={classNames(styles.description__change, change > 0 ? styles.positiveValue : styles.negativeValue) + ' ' + animationClass[2]}>
                     {change > 0 ? '+' + change : change}
                 </span>
             </div>
